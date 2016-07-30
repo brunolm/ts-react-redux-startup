30de4a05b5dfc41796dc75f66d4109d334104cdd
Bruno Leonardo Michels <brunolm7@gmail.com>
2016-07-30 18:24:15 -0300

    Remove obsolete typings folder

--- typings/react-redux/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@types/react-redux';
-
-export interface ProviderProps {
-  store?: Store<any>;
-  children?: ReactNode;
-}
-export declare class Provider extends Component<ProviderProps, {}> { }
