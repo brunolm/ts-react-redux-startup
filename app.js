1c6be9a1c3ffda785e05b7e563226f0b14e744cc
Bruno Leonardo Michels <brunolm7@gmail.com>
2016-08-01 00:15:13 -0300

    Add webpack-dev-middleware

--- app.js
+++ app.js
@@ -3,11 +3,30 @@ const vash = require('vash');
 const express = require('express');
 const favicon = require('serve-favicon');
 const compression = require('compression');
+const webpack = require('webpack');
+
+const webpackConfig = require('./webpack.config.development');
+
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  const compiler = webpack(webpackConfig);
+
+  console.log('Using dev-middleware');
+
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: false,
+    publicPath: webpackConfig.output.publicPath,
+    stats: {
+      colors: true
+    }
+  }));
+}
 
 app.use(compression());
 
 app.use(favicon(path.join(__dirname, 'favicon.ico')));
+
 app.use('/static', express.static(path.join(__dirname, 'dist')));
 
 app.set('view engine', 'vash');
